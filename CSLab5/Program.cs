3bea261887fed9a44e8fd36b2cf74ec37618c5ce
JakeSmokie <jakesmokie@gmail.com>
2018-04-02 10:02:51 +0300

    blank

--- CSLab5/Program.cs
+++ CSLab5/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
 
 namespace CSLab5
 {
-    internal class Program
+    class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-
+            BuildAvaloniaApp().Start<MainWindow>();
         }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug();
     }
 }
